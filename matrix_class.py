055d25cf7d77f65bfa67d5cec13d104c7e2eab7c
Vaskar <vaskar.nath@mail.utoronto.ca>
2020-04-24 15:13:13 -0600

    Created vector and matrix classes

---
+++ matrix_class.py
@@ -0,0 +1,11 @@
+
+class Matrix:
+
+    def __init__(self):
+        pass
+
+    def row_reduce(self):
+        pass
+
+    def multiply(self, matrix: Matrix):
+        pass
---
+++ vector_class.py
@@ -0,0 +1,13 @@
+# A vector class to represent vectors of any dimension
+
+
+class Vector:
+
+    def __init__(self, array):
+        pass
+
+    def dot_product(self):
+        pass
+
+    def scale(self):
+        pass
